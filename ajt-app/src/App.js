a4952e44d761f847e59162669d932ebbe883ac63
Atekkey <anand@tekkey.net>
2025-01-04 23:39:09 -0500

    Images added. Colors 𝚫, cleaner. Format Icons

--- ajt-app/src/App.js
+++ ajt-app/src/App.js
@@ -12,20 +12,22 @@ function App() {
       
       </section>
       <section class="section2">
-      Project 1
+      <div class="head1"> Minesweeper </div>
         <div class="container">
-          
-          <div class="column"> {/* Left Column */}
-            <ul>
-              <li><span> A </span></li>
-              <li><span> A </span></li>
-              <li><span> A </span></li>
-            </ul>
-
-
+          <div class="columnLeft"> {/* Left Column */}
+            • Built the classic game Minesweeper using Python 
+              and Object Oriented Programming <br></br> 
+            • Developed a prototype version in Terminal, and a UI based final version  <br></br>
+            • Implemented advanced features such as flood-filling 0's, Chording, & Difficulty Levels <br></br>
           </div>
 
-          <div class="column">Pictures here</div>
+          <div class="column">
+            <img src="PNGs/MSterminal.png" alt="Minesweeper in Terminal" width="180" height="150"/> 
+            <img src="PNGs/MS1.png" alt="Minesweeper Ex 2" width="220" height="180"/>
+            </div>
+          <div class="columnRight" >
+          <img src="PNGs/MS2.png" alt="Minesweeper Ex 3" width="280" height="220"/>
+          </div>
         </div>
       
       </section>

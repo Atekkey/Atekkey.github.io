f83548cfa53e785f340b6e811fa76461c5c8e46c
Atekkey <anand@tekkey.net>
2025-06-04 18:14:42 -0400

    New home adds

--- ajt-app/src/pages/SimpleHome.js
+++ ajt-app/src/pages/SimpleHome.js
@@ -3,42 +3,51 @@ import { Link } from 'react-router-dom';
 
 const SimpleHome = () => {
   return (
-
-    <div className="homebg">
-      
-      <div class="container">
-
-        <div class="column"> 
-          <mainHead><doubleFont>Hello!</doubleFont><br></br> My name is <colName>Anand Tekkey</colName>,<br></br> and this is my Portfolio.</mainHead>
-        </div>
-
-        <div class="column">
-            <img src="PNGs/anandFace.JPG" alt="Anand Tekkey" style={{ width: "30vw", height: "auto" }} />
-        </div>
-
-
+    <div className="app">
+      <div class="app-header">
+        Home
       </div>
-      
-      <div class="container">
+    
+      <div className="homebg">
         
-        <div class="column">
-            <div className="resumebox" >
-                    <Link to="/Resume" className="link" style={{color: "#1a1a1afa"}}>Resume </Link>
-            </div>
-        </div>
+        <div class="container">
+          <div class="column"> 
+            <mainHead><doubleFont>Hello!</doubleFont><br></br> My name is <colName>Anand Tekkey</colName>,<br></br> and this is my Portfolio.</mainHead>
+          </div>
 
-        <div class="column">
-          <a href = "https://github.com/atekkey"> <img src= "PNGs/GH3_edit.png" alt="github.com/atekkey" style={{ width: "10vw", height: "auto"}}></img> </a>
+          <div class="column">
+              <img src="PNGs/anandFace.JPG" alt="Anand Tekkey" style={{ width: "30vw", height: "auto" }} />
+          </div>
+
         </div>
-        <div class="column" >
-          <a href = "https://linkedin.com/in/atekkey"> <img src= "PNGs/LI_edit.png" alt="linkedin.com/in/atekkey" style={{ width: "10vw", height: "auto" }}></img> </a>
+        
+        <div class="container">
+          
+          <div class="column">
+              <div className="resumebox" >
+                      <Link to="/Resume" className="link" style={{color: "#000000FF"}}>Resume </Link>
+              </div>
+          </div>
+
+          <div class="column" >
+          <a href = "mailto:anand@tekkey.net"> <img src= "PNGs/gmail2_edit.png" alt="anand@tekkey.net" style={{ width: "10vw", height: "10vw", marginLeft:"-2vw"}}></img> </a>
+          </div>
+
+          <div class="column" >
+            <img src= "PNGs/uiuc1_edit.png" alt="UIUC Logo" style={{ width: "15vw", height: "15vw", margin:"-4vw"}} />
+          </div>
+
+          <div class="column">
+            <a href = "https://github.com/atekkey"> <img src= "PNGs/GH3_edit.png" alt="github.com/atekkey" style={{ width: "10vw", height: "10vw", marginRight:"-2vw"}}></img> </a>
+          </div>
+          <div class="column" >
+            <a href = "https://linkedin.com/in/atekkey"> <img src= "PNGs/LI_edit.png" alt="linkedin.com/in/atekkey" style={{ width: "10vw", height: "10vw" }}></img> </a>
+          </div>
+
         </div>
 
       </div>
 
-
-
-      
 
     </div>
   );
--- edit_png.py
+++ edit_png.py
@@ -1,17 +1,25 @@
 from PIL import Image
 
-img = Image.open("./ajt-app/public/PNGs/LI.png").convert("RGBA")
+img = Image.open("./ajt-app/public/PNGs/uiuc1.png").convert("RGBA")
 pixels = img.load()
 
 
-new_color = (0xFA, 0xFA, 0xFA, 0x20)
+new_color = (0xFA, 0xFA, 0xFA, 0x00)
+new_color2 = (0x00, 0x00, 0x00, 0xfa)
 
 width, height = img.size
 for y in range(height):
     for x in range(width):
         r, g, b, a = pixels[x, y]
-        if (r, g, b, a) != (0, 0, 0, 255):
+        if r*g*b > 30**3:
             pixels[x, y] = new_color
+        else:
+            pixels[x, y] = new_color2
+        #     continue
+        # if r > 20 + g and r > 20 + b:
+        #     pixels[x, y] = (0x00, 0x00, 0x00, 0xFF)
+        # else:
+        #     pixels[x, y] = (r, g, b, 0x3a)
 
 # Save result
-img.save("./ajt-app/public/PNGs/LI_edit.png")
+img.save("./ajt-app/public/PNGs/uiuc1_edit.png")
